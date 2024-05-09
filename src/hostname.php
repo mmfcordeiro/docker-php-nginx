e618ea0ea481d3ec0286f8085afd33855ac6a8b4
Mário Cordeiro <mario.miguel.cordeiro@gmail.com>
2024-05-09 15:49:12 +0100

    Add files via upload
    added hostname php

---
+++ src/hostname.php
@@ -0,0 +1,30 @@
+<?php
+// Function to get the client IP address
+function get_client_ip() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+$ip = get_client_ip();
+$host = gethostbyaddr($ip);
+$obj = (object) [
+    'ip' => $ip,
+    'host' => $host
+];
+header('Content-type: application/json');
+header("Access-Control-Allow-Origin: *");
+echo json_encode($obj);
+?>
